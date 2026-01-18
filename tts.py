f3c521f7aea99674ab7d9f23d0c3a46a9f823e68
Eranga <erangakavishanka@icloud.com>
2026-01-18 10:38:42 +0530

    developed the functioj

--- tts.py
+++ tts.py
@@ -6,4 +6,13 @@ from playsound import playsound
 
 
 def speak_text():
-  
+    text = text_input.get("1.0", tk.END).strip()
+    if text:
+        tts = gTTS(text=text, lang="en")
+        tts.save("output.mp3")
+        playsound("output.mp3")
+
+
+
+
+
